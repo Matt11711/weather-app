f9562be75164ba13f2840f9585404289aa4bee4e
Matt11711 <matthew.d.allen97@gmail.com>
2022-03-19 15:09:18 -0400

    added local storage and full card functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,16 @@ http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=61d65336c26f
 var currentWeatherEl = document.querySelector("#current-weather")
 var dailyForecastEl = document.querySelector("#daily-forecast")
 var savedCitiesEl = document.querySelector("#saved-cities")
+var searchButton = document.querySelector("#search-button")
+cityArray = []
+
 var getLatLon = function(city) {
     var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=61d65336c26f3aab8d79e21de8c8e05c";
   
     fetch(apiUrl).then(function(response) {
       if (response.ok) {
         response.json().then(function(data) {
-          getWeather(data[0].lat, data[0].lon);
+          getWeather(data[0].lat, data[0].lon,city);
         })} else {
         alert('Error: City Not Found');
       }
@@ -18,23 +21,104 @@ var getLatLon = function(city) {
   };
 
 
-var getWeather = function(lat,lon) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=61d65336c26f3aab8d79e21de8c8e05c";
+var getWeather = function(lat,lon,city) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=61d65336c26f3aab8d79e21de8c8e05c";
   
     fetch(apiUrl).then(function(response) {
       if (response.ok) {
         response.json().then(function(data) {
-          todayWeather(data)
+            currentWeatherEl.innerHTML=""
+    dailyForecastEl.innerHTML = ""
+          todayWeather(data,city)
           dailyForecast(data)
+          saveSearch(city)
         })} else {
         alert('Error: City Not Found');
       }
     });
   };
 
-function todayWeather(data) {
+function todayWeather(data,city) {
 var weatherTitle = document.createElement("h2")
+var temp = document.createElement("p")
+var wind = document.createElement("p")
+var humidity = document.createElement("p")
+var uv = document.createElement("p")
+console.log(data.current.dt)
+var currentDate = new Date(data.current.dt*1000)
+console.log(currentDate)
+weatherTitle.innerHTML = city +" (" + (currentDate.getMonth()+1)+ "/"+currentDate.getDate() + "/"+currentDate.getFullYear()+ ")" + "<img src=' http://openweathermap.org/img/wn/" + data.current.weather[0].icon + ".png'/>"
+currentWeatherEl.appendChild(weatherTitle)
 
+temp.textContent = "Temp: " + data.current.temp + "°F"
+currentWeatherEl.appendChild(temp)
+wind.textContent = "Wind: " + data.current.wind_speed + " MPH"
+currentWeatherEl.appendChild(wind)
+humidity.textContent = "Humidity: " + data.current.humidity + " %"
+currentWeatherEl.appendChild(humidity)
+uv.textContent = "UV Index: " 
+var uvData = document.createElement("span")
+uvData.textContent = data.current.uvi
+uv.append(uvData)
+currentWeatherEl.appendChild(uv)
 }
 
-getLatLon('london')
+function dailyForecast(data) {
+   var sectionTitle = document.createElement("h3")
+   sectionTitle.textContent = "5-Day Forecast:"
+   dailyForecastEl.appendChild(sectionTitle)
+    for (i=1;i<6;i++) {
+        var weatherCard = document.createElement("div")
+        var currentDate = new Date(data.daily[i].dt*1000)
+      
+var todayDateEl = document.createElement("h4")
+var weatherIcon = document.createElement("img")
+var temp = document.createElement("p")
+var wind = document.createElement("p")
+var humidity = document.createElement("p")
+todayDateEl.textContent = (currentDate.getMonth()+1)+ "/"+currentDate.getDate() + "/"+currentDate.getFullYear()
+
+weatherCard.appendChild(todayDateEl)
+weatherIcon.src= "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png"
+weatherCard.appendChild(weatherIcon)
+temp.textContent = "Temp: " + data.current.temp + "°F"
+weatherCard.appendChild(temp)
+wind.textContent = "Wind: " + data.current.wind_speed + " MPH"
+weatherCard.appendChild(wind)
+humidity.textContent = "Humidity: " + data.current.humidity + " %"
+weatherCard.appendChild(humidity)
+        dailyForecastEl.appendChild(weatherCard)
+    }
+}
+
+function saveSearch(city) {
+   
+    
+    cityArray.push(city)
+    localStorage.setItem("city",JSON.stringify(cityArray))
+}
+
+function addSavedCity(city) {
+    var cityEl = document.createElement("p")
+    cityEl.textContent=city
+    savedCitiesEl.appendChild(cityEl)
+}
+function loadSavedCities() {
+var savedCities = JSON.parse(localStorage.getItem("city"))
+cityArray = savedCities
+for (i=0;i<savedCities.length;i++) {
+    addSavedCity(savedCities[i])
+}
+}
+searchButton.onclick = function(event){
+    event.preventDefault();
+    var cityName = document.querySelector("#city-name-input").value
+    addSavedCity(cityName)
+    
+    getLatLon(cityName)
+    document.querySelector("#city-name-input").value=''
+}
+savedCitiesEl.onclick = function(event) {
+    getLatLon(event.target.textContent)
+}
+loadSavedCities()
